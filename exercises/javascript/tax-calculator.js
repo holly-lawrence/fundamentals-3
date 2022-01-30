4ca609532c4225f7b4dfd315eed0e2d43ff16527
Holly Lawrence <holly.lawrence@bjss.com>
2022-01-30 19:11:12 +0000

    initial payment calculator

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -12,8 +12,33 @@ let TaxCalculator = class TaxCalculator {
     return this.year;
   }
 
+  calculateInitialPayment(fuelType, co2Emissions) {
+    const co2_bounds = [0,1,51,76,91,101,111,131,151,171,191,226,256];
+
+    const init_prices = {
+        'petrol': [0,10,25,105,125,145,165,205,515,,830,1240,1760,2070],
+        'diesel': [0,25,105,125,145,165,205,515,830,1240,1760,2070,2070],
+        'alternative-fuel': [0,0,15,95,115,135,155,195,505,820,1230,1750,2060]
+        };
+
+    const co2_bound = co2_bounds.find(x => x > co2Emissions) - 1;
+
+    const initial_price = init_prices[`${fuelType}`][co2_bound];
+
+    return initial_price;
+  }
+
+
   calculateTax(vehicle) {
-    throw new Error('You have to implement the method doSomething!');
+    // Find Car Age
+
+
+    // Find Price
+    initial_price = calculateInitialPayment(vehicle.fuelType, vehicle.co2Emissions);
+
+    return initial_price
+
+
   }
 }
 
