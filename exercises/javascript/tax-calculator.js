e31a4c3b5ca9005fbb217949cce0495d98a12ba9
Holly Lawrence <holly.lawrence@bjss.com>
2022-01-31 16:38:31 +0000

    refactored electric array

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -19,7 +19,7 @@ let TaxCalculator = class TaxCalculator {
         'Petrol': [0,10,25,105,125,145,165,205,515,830,1240,1760,2070],
         'Diesel': [0,25,105,125,145,165,205,515,830,1240,1760,2070,2070],
         'Alternative fuel': [0,0,15,95,115,135,155,195,505,820,1230,1750,2060],
-        'Electric': [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        'Electric': new Array(13).fill(0)
         };
 
     const co2_bound = co2_bounds.findIndex(x => x > co2Emissions) - 1;
